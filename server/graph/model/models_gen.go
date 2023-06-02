49bf9fd02ab1668b2558ad78d6eb9efacad2ae0b
yuda-miro <ansemo36fe@gmail.com>
2023-06-03 02:44:26 +0900

    delete: firebaseID

--- server/graph/model/models_gen.go
+++ server/graph/model/models_gen.go
@@ -18,7 +18,7 @@ type CreateScheduleInput struct {
 }
 
 type CreateUserInput struct {
-	FirebaseID  string `json:"firebase_id"`
+	ID          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
@@ -75,14 +75,12 @@ type UpdateScheduleInput struct {
 
 type UpdateUserInput struct {
 	ID          string `json:"id"`
-	FirebaseID  string `json:"firebase_id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
 
 type User struct {
 	ID          string `json:"id"`
-	FirebaseID  string `json:"firebase_id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
