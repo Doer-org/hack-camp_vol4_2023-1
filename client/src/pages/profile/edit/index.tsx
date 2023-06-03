c3a3976557aeca02c55e3b5989f5ac636270ac8f
Mao Yamaguchi <maomao.xmdhkdltm@gmail.com>
2023-06-03 21:06:50 +0900

    fix:prettier

--- client/src/pages/profile/edit/index.tsx
+++ client/src/pages/profile/edit/index.tsx
@@ -3,20 +3,17 @@ import { Title } from "@/components/elements/Title";
 import { RootLayout } from "@/components/layout/Layout";
 import { EditForm } from "@/components/pages/Profile/Edit/edit-form";
 
-
 const Edit = () => {
   return (
     <RootLayout meta="ユーザー情報を編集する">
-    <div className="p-10 py-20 h-screen">
-      <Title>
-        ユーザー情報編集
-      </Title>
-      <div>
-        <EditForm />
+      <div className="p-10 py-20 h-screen">
+        <Title>ユーザー情報編集</Title>
+        <div>
+          <EditForm />
+        </div>
       </div>
-    </div>
-  </RootLayout>
-  )
+    </RootLayout>
+  );
 };
 
 export default Edit;
