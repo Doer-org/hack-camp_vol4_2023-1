deee8ecf97aa3b65d6bce12bbac3c8d789b97176
Mao Yamaguchi <102772040+Meow520@users.noreply.github.com>
2023-08-10 14:41:09 +0900

    Delete client/src/components/pages/Home directory

--- client/src/components/pages/Home/home-main.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React, { FC } from "react";
-import { HomeAfterLogin } from "@/components/pages/home/home-after-login";
-
-export const HomeComponent:FC = () => {
-  return <HomeAfterLogin />;
-};
