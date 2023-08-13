c2672f9052cf0bda3e96df2b3e5edc074ee9be31
ogawakenta <kentaogawa1123@gmail.com>
2023-08-13 20:42:17 +0900

    hangoutAPIの作成

--- server/internal/infrastructure/repository/hangout.go
+++ server/internal/infrastructure/repository/hangout.go
@@ -2,9 +2,11 @@ package repository
 
 import (
 	"context"
+
 	"github.com/Doer-org/hack-camp_vol4_2023-1/internal/domain/entity"
 	"github.com/Doer-org/hack-camp_vol4_2023-1/internal/domain/repository"
 	"github.com/Doer-org/hack-camp_vol4_2023-1/internal/infrastructure/database"
+	d "github.com/Doer-org/hack-camp_vol4_2023-1/internal/infrastructure/repository/dto"
 )
 
 var _ repository.IHangoutRepository = &HangoutReposiroty{}
@@ -19,25 +21,42 @@ func NewHangoutRepository(conn *database.Conn) repository.IHangoutRepository {
 	}
 }
 
-// comment: friend.goを参照にして実装してください。
-// comment: 実装後はコメントアウトされているのを全て消してください
-// CreateHangout implements repository.IHangoutRepository.
-func (*HangoutReposiroty) CreateHangout(ctx context.Context, hangout *entity.Hangout) (*entity.Hangout, error) {
-	panic("unimplemented")
+func (ur *HangoutReposiroty) CreateHangout(ctx context.Context, hangout *entity.Hangout) (*entity.Hangout, error) {
+	query := `INSERT INTO friend (id, user_id, name) VALUES (:id, :user_id, :name);`
+	dto := d.HangoutEntityToDto(hangout)
+	_, err := ur.conn.DB.NamedExecContext(ctx, query, dto)
+	if err != nil {
+		return nil, err
+	}
+	return d.HangoutDtoToEntity(&dto), nil
 }
 
-// DeleteHangout implements repository.IHangoutRepository.
-func (*HangoutReposiroty) DeleteHangout(ctx context.Context, id string) error {
-	panic("unimplemented")
+func (ur *HangoutReposiroty) DeleteHangout(ctx context.Context, id string) error {
+	query := `DELETE FROM hangout WHERE id = ?;`
+	_, err := ur.conn.DB.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-// GetHangoutsByUserId implements repository.IHangoutRepository.
-func (*HangoutReposiroty) GetHangoutsByUserId(ctx context.Context, id string) (entity.Hangouts, error) {
-	panic("unimplemented")
+func (ur *HangoutReposiroty) GetHangoutsByUserId(ctx context.Context, id string) (entity.Hangouts, error) {
+	query := `SELECT * FROM hangout WHERE id = ?`
+	var dtos d.HangoutsDtos
+	err := ur.conn.DB.SelectContext(ctx, &dtos, query, id)
+	if err != nil {
+		return nil, err
+	}
+	return d.HangoutsDtosToEntity(dtos), nil
 }
 
-// UpdateHangout implements repository.IHangoutRepository.
-func (*HangoutReposiroty) UpdateHangout(ctx context.Context, hangout *entity.Hangout, id string) (*entity.Hangout, error) {
-	panic("unimplemented")
+func (ur *HangoutReposiroty) UpdateHangout(ctx context.Context, hangout *entity.Hangout, id string) (*entity.Hangout, error) {
+	query := `UPDATE hangout SET name = :name WHERE id = :id`
+	dto := d.HangoutEntityToDto(hangout)
+	dto.Id = id
+	_, err := ur.conn.DB.NamedExecContext(ctx, query, &dto)
+	if err != nil {
+		return nil, err
+	}
+	return d.HangoutDtoToEntity(&dto), nil
 }
-
