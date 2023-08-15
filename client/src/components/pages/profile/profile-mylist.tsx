5c25057613ca12452bd8a50191656f6cccc80867
Meow520 <cgjg0289@mail4.doshisha.ac.jp>
2023-08-15 23:02:04 +0900

    husky test

--- client/src/components/pages/profile/profile-mylist.tsx
+++ client/src/components/pages/profile/profile-mylist.tsx
@@ -11,7 +11,8 @@ export const ProfileMylist: FC<ProfileMylistProps> = ({ hangouts, schedules }) =
   console.log(hangouts);
   return (
     <div className="bg-new-white shadow-md rounded-lg p-4">
-      {/* <div>
+      <div>
+        {/*
         <div className="grid grid-cols-3 gap-4 py-4 pb-8 border-b border-navy-3">
           {hangouts != null ? (
             hangouts.map((hangout, index) => (
@@ -36,8 +37,8 @@ export const ProfileMylist: FC<ProfileMylistProps> = ({ hangouts, schedules }) =
                 </div>
               );
             })}
-        </div>
-      </div> */}
+          </div> */}
+      </div>
     </div>
   );
 };
