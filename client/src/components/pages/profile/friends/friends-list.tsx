b15ba39d232924638a0134d4a9116e0690c537b2
Meow520 <cgjg0289@mail4.doshisha.ac.jp>
2023-08-15 22:57:42 +0900

    husky test

--- client/src/components/pages/profile/friends/friends-list.tsx
+++ client/src/components/pages/profile/friends/friends-list.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { LinkButton } from "@/components/elements/Button";
 import { Text } from "@/components/elements/Text";
 import { Title } from "@/components/elements/Title";
 
 export const FriendsList = () => {
+  const friend = { id: "1" };
   return (
     <div>
       <div className="py-5">
@@ -10,10 +12,10 @@ export const FriendsList = () => {
       </div>
       <div>
         <hr />
-        <div className="flex gap-4 py-4">
+        <LinkButton className="flex gap-4 py-4" href={`/friend/${friend.id}`}>
           <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
           <Text>Name</Text>
-        </div>
+        </LinkButton>
       </div>
     </div>
   );
