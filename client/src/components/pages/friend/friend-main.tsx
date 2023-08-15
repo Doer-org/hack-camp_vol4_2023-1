3a376243d53c31a30493bd308e8650476db70439
Meow520 <cgjg0289@mail4.doshisha.ac.jp>
2023-08-15 22:40:14 +0900

    husky test

--- client/src/components/pages/friend/friend-main.tsx
+++ client/src/components/pages/friend/friend-main.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { FriendOverview } from '@/components/pages/friend/friend-overview'
+import { Friend } from '@/api/friend/type'
+import { Hangout } from '@/api/hangout/type'
+import { Schedule } from '@/api/schedule/type'
+import { FriendMylist } from '@/components/pages/friend/friend-mylist'
 
-export const FriendMain = () => {
+type FriendMainProps = {
+  friend: Friend
+  hangouts: Hangout[],
+  schedules:Schedule[]
+}
+
+export const FriendMain:FC<FriendMainProps> = ({friend, hangouts, schedules}) => {
   return (
-    <div>FriendMain</div>
+    <div className="user-bg py-16 h-screen">
+    <div className="w-[320px] mx-auto">
+      <FriendOverview friend={friend} />
+      <FriendMylist hangouts={hangouts} schedules={schedules} />
+    </div>
+  </div>
   )
 }
