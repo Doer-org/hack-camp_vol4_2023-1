864c23d190a4e50868d5a88e52d9b294e8a4adff
Meow520 <cgjg0289@mail4.doshisha.ac.jp>
2023-08-15 23:10:38 +0900

    husky test

--- client/src/pages/create/schedule/index.tsx
+++ client/src/pages/create/schedule/index.tsx
@@ -1,29 +1,13 @@
 import { GetServerSideProps, NextPage } from "next";
 import { parseCookies } from "nookies";
 import React from "react";
-import { Text } from "@/components/elements/Text";
-import { Title } from "@/components/elements/Title";
 import { RootLayout } from "@/components/layout/Layout";
-import { ScheduleForm } from "@/components/pages/create/schedule/schedule-form";
+import { ScheduleMain } from "@/components/pages/create/schedule/schedule-main";
 
 const Schedule: NextPage = () => {
   return (
     <RootLayout meta="予定を登録する">
-      <div className="schedule-bg p-10 py-20 h-screen">
-        <div className="py-4">
-          <Title>
-            予定を
-            <br />
-            登録しましょう！
-          </Title>
-        </div>
-        <div className="pt-20 py-4">
-          <Text>ooさんはいつ遊びたいですか？</Text>
-        </div>
-        <div>
-          <ScheduleForm />
-        </div>
-      </div>
+      <ScheduleMain />
     </RootLayout>
   );
 };
