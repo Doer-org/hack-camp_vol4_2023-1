4abd42053ab8e37c0ada9da7bad2983dbe5081a1
ogawakenta <kentaogawa1123@gmail.com>
2023-08-26 09:44:47 +0900

    FriendAPI確認しました

---
+++ client/src/components/pages/Login 6/login-form.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import auth from "@/firebase/client";
+import React, { FC } from "react";
+import { signIn as signInByNextAuth } from "next-auth/react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { Button } from "@/components/elements/Button";
+import { RiGoogleFill } from "react-icons/ri";
+import { CreateUser } from "@/api/mutation";
+
+export const LoginForm: FC = () => {
+  const login = async () => {
+    const provider = new GoogleAuthProvider();
+    const cred = await signInWithPopup(auth, provider);
+    const userData = {
+      id: cred.user.uid,
+      name: cred.user.displayName ? cred.user.displayName : "",
+      description: "",
+    };
+
+    const { data, err } = await CreateUser(userData);
+    if (err) {
+      console.log("Error:", err);
+    }
+    console.log(data);
+
+    localStorage.setItem("user", JSON.stringify(userData));
+    // const idToken = await cred.user.getIdToken();
+    // await signInByNextAuth("credentials", {
+    //   idToken,
+    //   callbackUrl: "/",
+    // });
+  };
+  return (
+    <div className="w-60 mx-auto">
+      <Button
+        className="bg-new-blue-700 text-new-white text-[20px] flex justify-center gap-3 items-center w-60 h-16 rounded-lg px-10"
+        onClick={login}
+      >
+        <RiGoogleFill className="w-7 h-7" />
+        Log in
+      </Button>
+    </div>
+  );
+};
