8e0aa66acf74130f4d33e4fd0bafe0f2e0c67ded
Meow520 <cgjg0289@mail4.doshisha.ac.jp>
2023-09-01 05:00:07 +0900

    [fix]cors error

--- server/web/router/router.go
+++ server/web/router/router.go
@@ -40,6 +40,8 @@ func (r *Router) cors() {
 		// アクセスを許可したいアクセス元
 		AllowOrigins: []string{
 			"http://localhost:3000",
+			"https://hack-camp-vol4-2023-1-qqg93k4lw-hack-camp-vol4-2023-1.vercel.app/",
+			"https://hack-camp-vol4-2023-1.vercel.app/",
 		},
 		// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
 		AllowMethods: []string{
